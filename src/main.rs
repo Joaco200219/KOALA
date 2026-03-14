cbf4ace595aa059ce1d2118dc7ba65090ee36cd4
Joaco200219 <salamecondulcedeleche@gmail.com>
2026-03-14 10:01:11 -0300

    Lectura de archivos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use serde_json;
+use std::fs;
+
+#[derive(Deserialize, Debug)]
+struct IPS {
+    nombre: String,
+    ip4: Vec<String>,
+    ip6: Vec<String>,
+    intervalo_segundos: u8,
 }
+
+fn main(){
+    //Leer datos
+    let datos = fs::read_to_string("ips.json").expect("PROBLEMA: lectura del archivo");
+    let objetivos: Vec<IPS> = serde_json::from_str(&datos)
+    .expect("PROBLEMA: con los datos");
+
+    //Muestreo lectura
+    for objetivo in objetivos{
+        println!("=============Lectura de Datos============");
+        println!("Nombre: {}\nIntervalo: {}",objetivo.nombre, objetivo.intervalo_segundos);
+        println!("IPv4: {} y {}", objetivo.ip4[0], objetivo.ip4[1]);
+        println!("IPv6: {} y {}", objetivo.ip6[0], objetivo.ip6[1]);
+    }
+
+    println!("=================FIN======================");
+}
+
+
