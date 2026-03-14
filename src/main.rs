a5d43367fcc25462f893d1b8b49dc8b20cdcd14f
Joaco200219 <salamecondulcedeleche@gmail.com>
2026-03-14 19:32:50 -0300

    IPv4 y IPv6 exitosos

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,21 @@ struct IPS {
     nombre: String,
     ip4: Vec<String>,
     ip6: Vec<String>,
-    intervalo_segundos: u8,
 }
 
-#[derive(Debug)]
-struct Metricas {
-    min: f32,
+#[derive(Debug)]//Los parametros comentados no se utilizan, logica implementada
+struct Metricas {//si se desean utilizar, ya se extraen
+    //min: f32,
     avg: f32,
-    max: f32,
-    mdev: f32,
+    //max: f32,
+    mdev: f32,   
+    packet_loss: u8,
 }
 
 fn extraer_metricas(texto_salida: &str) -> Option<Metricas> {
+    let linea = texto_salida.lines().find(|l| l.contains("packet loss"))?;
+    let primera_mitad = linea.split("%").next()?;
+    let numero = primera_mitad.split_whitespace().last()?;
     let linea_metricas = texto_salida.lines().find(|linea| linea.contains("rtt min/avg/max/mdev"))?;
     let valores = linea_metricas.split("=").nth(1)?;
     let valores_limpios = valores.trim().trim_end_matches(" ms");
@@ -27,10 +30,11 @@ fn extraer_metricas(texto_salida: &str) -> Option<Metricas> {
 
     if partes.len() == 4 {
         Some(Metricas{
-            min: partes[0].parse().unwrap_or(0.0),
+            //min: partes[0].parse().unwrap_or(0.0),
             avg: partes[1].parse().unwrap_or(0.0),
-            max: partes[2].parse().unwrap_or(0.0),
+            //max: partes[2].parse().unwrap_or(0.0),
             mdev: partes[3].parse().unwrap_or(0.0),
+            packet_loss: numero.parse::<u8>().ok()?,
         })
     } else {
         None
@@ -59,14 +63,36 @@ fn main(){
                 .arg(ip)
                 .output()
                 .expect("Problema al ejecutar el comando");
-
             let texto_salida = String::from_utf8_lossy(&comando.stdout);
             if let Some(metricas) = extraer_metricas(&texto_salida) {
                 println!("Exito, media {} ms", metricas.avg);
+                println!("Media: {}ms", metricas.mdev);
+                println!("Paquetes perdidos: {}%", metricas.packet_loss);
             } else {
                 println!("PROBLEMA extrayendo datos");
             }
         }
+
+        for ip in objetivo.ip6{
+            println!("Ping a {}", ip);
+            let comando = Command::new("ping")
+                .arg("-c")
+                .arg("4")
+                .arg("-6") //Forzar IPv6
+                .arg(ip)
+                .output()
+                .expect("Problema al ejecutar el comando");
+            let texto_salida = String::from_utf8_lossy(&comando.stdout);
+            if let Some(metricas) = extraer_metricas(&texto_salida) {
+                println!("Exito, media {} ms", metricas.avg);
+                println!("Media: {}ms", metricas.mdev);
+                println!("Paquetes perdidos: {}%", metricas.packet_loss);
+            } else {
+                println!("PROBLEMA extrayendo datos");
+            }
+        }
+        println!("==================================================")
+
     }
 }
 
